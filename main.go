c42f7f79ca496f3afe21940956bb7eb61a675616
RioneSSL <rionesslq@gmail.com>
2022-01-19 12:16:51 +0900

    GOTEST22

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var (
 	mode = flag.String("m", "server", "mode: client or server")
 	port = flag.String("p", "20021", "host: ip:port")
-	MyId = 0
+	MyId int = 0
 )
 
 func main() {
@@ -40,7 +40,7 @@ func RunServer() {
 		err = proto.Unmarshal(buf[0:n], packet)
 		log.Printf("Received %d from %s", *packet.Commands, addr)
 
-		if *packet.Commands.Id() == *MyId {
+		if *packet.Commands.Robot_commands.Id() == *MyId {
 			log.Printf("Robot 0 Data Received")
 		}
 		if err != nil {
