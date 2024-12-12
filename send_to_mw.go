683ad714a9d4554ece006e49a87ed80135798bef
asoyusaku <is0708ve@ed.ritsumei.ac.jp>
2024-12-12 18:51:49 +0900

    change

--- send_to_mw.go
+++ send_to_mw.go
@@ -13,9 +13,9 @@ import (
 
 	picturepb "github.com/Rione/ssl-RACOON-Pi2/proto/pb_gen"
 
-	// "gobot.io/x/gobot" //required to play picamera
-	// "gobot.io/x/gobot/platforms/opencv" //required to play picamera
-	"gocv.io/x/gocv" //required to play picamera
+	"gobot.io/x/gobot"                  //required to play picamera
+	"gobot.io/x/gobot/platforms/opencv" //required to play picamera
+	"gocv.io/x/gocv"                    //required to play picamera
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	// "google.golang.org/protobuf/proto"
@@ -30,61 +30,6 @@ var (
 	id      uint32 = 1
 )
 
-// func ClientStream() {
-// ipv4 := "127.0.0.1"
-// port := "9999"
-// address := ipv4 + ":" + port
-
-// fmt.Println("Sender:", address)
-// conn, _:= net.Dial("udp", address)
-// // CheckError(err)
-// defer conn.Close()
-// 	runtime.LockOSThread()
-// 	window := opencv.NewWindowDriver()
-// 	camera := opencv.NewCameraDriver(0)
-
-// 	stream, err := client.ClientStream(context.Background())
-// 	if err != nil {
-// 		log.Fatalf("error while creating stream: %v", err)
-// 	}
-
-// 	work := func() {
-// 		camera.On(opencv.Frame, func(data interface{}) {
-// 			img := data.(gocv.Mat)
-// 			params := []int{gocv.IMWriteJpegQuality, 25}
-// 			buf, err := gocv.IMEncodeWithParams(".jpg", img, params)
-// 			if err != nil {
-// 				log.Fatalf("failed to encode image: %v", err)
-// 			}
-// 			if err := stream.Send(&picturepb.ImageRequest{
-// 				Image: buf.GetBytes(),
-// 				Id:    int32(id),
-// 			}); err != nil {
-// 				log.Fatalf("failed to send image: %v", err)
-// 			}
-
-// pe := &picturepb.ImageRequest{
-// 	Image: buf.GetBytes(),
-// }
-// Data, _ := proto.Marshal(pe)
-
-// conn.Write([]byte(Data))
-
-// conn.Write([]byte(Data))
-
-// 			// window.ShowImage(img)
-// 			window.WaitKey(1)
-// 		})
-// 	}
-
-// 	robot := gobot.NewRobot("cameraBot",
-// 		[]gobot.Device{window, camera},
-// 		work,
-// 	)
-
-// 	robot.Start()
-// }
-
 func ClientStream(id uint32) {
 	// ipv4 := "127.0.0.1"
 	// port := "9999"
@@ -94,53 +39,108 @@ func ClientStream(id uint32) {
 	// conn, _:= net.Dial("udp", address)
 	// // CheckError(err)
 	// defer conn.Close()
-
 	runtime.LockOSThread()
-	var err error
-	webcam, err = gocv.OpenVideoCapture(0)
-	if err != nil {
-		log.Fatalf("error while opening video capture: %v", err)
-	}
+	window := opencv.NewWindowDriver()
+	camera := opencv.NewCameraDriver(0)
+
 	stream, err := client.ClientStream(context.Background())
 	if err != nil {
 		log.Fatalf("error while creating stream: %v", err)
 	}
-	defer webcam.Close()
 
-	fmt.Println("Camera initialized successfully.")
+	work := func() {
+		camera.On(opencv.Frame, func(data interface{}) {
+			img := data.(gocv.Mat)
+			params := []int{gocv.IMWriteJpegQuality, 25}
+			buf, err := gocv.IMEncodeWithParams(".jpg", img, params)
+			if err != nil {
+				log.Fatalf("failed to encode image: %v", err)
+			}
+			if err := stream.Send(&picturepb.ImageRequest{
+				Image: buf.GetBytes(),
+				Id:    int32(id),
+			}); err != nil {
+				log.Fatalf("failed to send image: %v", err)
+			}
 
-	img := gocv.NewMat()
-	defer img.Close()
+			// pe := &picturepb.ImageRequest{
+			// 	Image: buf.GetBytes(),
+			// }
+			// Data, _ := proto.Marshal(pe)
 
-	for {
-		ok := webcam.Read(&img)
-		if !ok {
-			log.Fatalf("cannot read device %v\n", 0)
-		}
+			// conn.Write([]byte(Data))
 
-		params := []int{gocv.IMWriteJpegQuality, 25}
-		buf, err := gocv.IMEncodeWithParams(".jpg", img, params)
-		if err != nil {
-			log.Fatalf("failed to encode image: %v", err)
-		}
+			// conn.Write([]byte(Data))
 
-		if err := stream.Send(&picturepb.ImageRequest{
-			Image: buf.GetBytes(),
-			Id:    1,
-		}); err != nil {
-			log.Fatalf("failed to send image: %v", err)
-			break
-		}
-
-		// pe := &picturepb.ImageRequest{
-		// 	Image: buf.GetBytes(),
-		// }
-		// Data, _ := proto.Marshal(pe)
-
-		// conn.Write([]byte(Data))
-
+			// window.ShowImage(img)
+			window.WaitKey(1)
+		})
 	}
+
+	robot := gobot.NewRobot("cameraBot",
+		[]gobot.Device{window, camera},
+		work,
+	)
+
+	robot.Start()
 }
+
+// func ClientStream(id uint32) {
+// 	// ipv4 := "127.0.0.1"
+// 	// port := "9999"
+// 	// address := ipv4 + ":" + port
+
+// 	// fmt.Println("Sender:", address)
+// 	// conn, _:= net.Dial("udp", address)
+// 	// // CheckError(err)
+// 	// defer conn.Close()
+
+// 	runtime.LockOSThread()
+// 	var err error
+// 	webcam, err = gocv.OpenVideoCapture(0)
+// 	if err != nil {
+// 		log.Fatalf("error while opening video capture: %v", err)
+// 	}
+// 	stream, err := client.ClientStream(context.Background())
+// 	if err != nil {
+// 		log.Fatalf("error while creating stream: %v", err)
+// 	}
+// 	defer webcam.Close()
+
+// 	fmt.Println("Camera initialized successfully.")
+
+// 	img := gocv.NewMat()
+// 	defer img.Close()
+
+// 	for {
+// 		ok := webcam.Read(&img)
+// 		if !ok {
+// 			log.Fatalf("cannot read device %v\n", 0)
+// 		}
+
+// 		params := []int{gocv.IMWriteJpegQuality, 25}
+// 		buf, err := gocv.IMEncodeWithParams(".jpg", img, params)
+// 		if err != nil {
+// 			log.Fatalf("failed to encode image: %v", err)
+// 		}
+
+// 		if err := stream.Send(&picturepb.ImageRequest{
+// 			Image: buf.GetBytes(),
+// 			Id:    1,
+// 		}); err != nil {
+// 			log.Fatalf("failed to send image: %v", err)
+// 			break
+// 		}
+
+// 		// pe := &picturepb.ImageRequest{
+// 		// 	Image: buf.GetBytes(),
+// 		// }
+// 		// Data, _ := proto.Marshal(pe)
+
+// 		// conn.Write([]byte(Data))
+
+// 	}
+// }
 
 func Streaming(chstreaming chan bool, MyID uint32) {
 	fmt.Println("start gRPC Client.")
